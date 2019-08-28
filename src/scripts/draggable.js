6aa1ba6b18af94a5d10b40a91cf8c7538369da52
sunchao199606 <17854230810@163.com>
2019-08-28 09:36:34 +0800

    修改为web工程

--- src/scripts/draggable.js
+++ src/scripts/draggable.js
@@ -6,12 +6,6 @@ draggable.on('mousedown', () => {
     isMouseDown = true;
 })
 
-$(document).on('mousemove', (e) => {
-    if (isMouseDown) {
-        draggable.css('left', e.pageX + 'px');
-        draggable.css('top', e.pageY + 'px');
-    }
-})
 
 draggable.on('mouseup', () => {
     isMouseDown = false;
