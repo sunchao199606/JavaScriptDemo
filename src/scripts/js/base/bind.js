c3da0c4a4caa4289bec0d992d7d33d6d81913064
sunchao199606 <17854230810@163.com>
2020-11-10 16:42:37 +0800

    function.bind()

--- src/scripts/js/base/bind.js
+++ src/scripts/js/base/bind.js
@@ -3,9 +3,11 @@ let add = function (a, b) {
     console.log(this)
     return a + b
 }
+// 绑定之前this指向全局对象
 add(10, 20)
 let obj = {
     name: "dad"
 }
 let fun = add.bind(obj)
+// 绑定后this指向所绑定的对象
 fun(10, 20)
